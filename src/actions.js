e7b63fc829670ef325ebe26eac4a1cf02b5e4159
krishna9788 <krishnakushwaha978@gmail.com>
2023-04-04 23:46:01 +0530

    changedone

--- src/actions.js
+++ src/actions.js
@@ -12,8 +12,27 @@ import store from './store'
 
 
 
-const getUserById = async () => {
+const getUserById = async (id) => {
    //Complete this function
+     try {
+       // Dispatch GET_USERS_BY_ID_REQUEST action
+       store.dispatch({ type: GET_USERS_BY_ID_REQUEST });
+
+       // Fetch user data from the API
+       const response = await getUsers(id);
+
+       // Dispatch GET_USERS_BY_ID_SUCCESS action with the user's name as the payload
+       store.dispatch({
+         type: GET_USERS_BY_ID_SUCCESS,
+         payload: response.data.name,
+       });
+     } catch (error) {
+       // Dispatch GET_USERS_BY_ID_FAIL action with the error message as the payload
+       store.dispatch({
+         type: GET_USERS_BY_ID_FAIL,
+         payload: error.message,
+       });
+     }
 };
 
 export default getUserById
--- src/reducers.js
+++ src/reducers.js
@@ -14,15 +14,37 @@ const initialState = {
 export default function appReducer(state = initialState, action) {
     
     switch (action.type) {
-        case GET_USERS_BY_ID_REQUEST: {
-            return {
-                ...state,
-                isLoading: true,
-                isCompleted: false,
-                error: null,
-                data:null
-            }
-        }
-        //Complete the rest of the cases
+      case GET_USERS_BY_ID_REQUEST: {
+        return {
+          ...state,
+          isLoading: true,
+          isCompleted: false,
+          error: null,
+          data: null,
+        };
+      }
+
+      case GET_USERS_BY_ID_SUCCESS: {
+        return {
+          ...state,
+          isLoading: false,
+          isCompleted: true,
+          error: false,
+          data: action.payload,
+        };
+      }
+
+      case GET_USERS_BY_ID_FAIL: {
+        return {
+          ...state,
+          isLoading: false,
+          isCompleted: true,
+          error: action.payload,
+          data: null,
+        };
+      }
+      default:
+        return state;
+      //Complete the rest of the cases
     }
 }
