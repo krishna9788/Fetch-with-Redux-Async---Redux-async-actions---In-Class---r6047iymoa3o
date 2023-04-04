351fff4a5bf286d781020b275dece645d68379ff
krishna9788 <krishnakushwaha978@gmail.com>
2023-04-05 00:12:08 +0530

    done

--- src/actions.js
+++ src/actions.js
@@ -14,20 +14,22 @@ import store from './store'
 
 const getUserById = async (id) => {
    //Complete this function
-     try {
+    try {
+        
        // Dispatch GET_USERS_BY_ID_REQUEST action
-       store.dispatch({ type: GET_USERS_BY_ID_REQUEST });
-
+        store.dispatch({ type: GET_USERS_BY_ID_REQUEST });
+        const userName =  await getUsers(id);
        // Fetch user data from the API
-       const response = await getUsers(id);
+        //  const response = await getUsers(id);
+        //  console.log("check user",userName);
 
        // Dispatch GET_USERS_BY_ID_SUCCESS action with the user's name as the payload
        store.dispatch({
          type: GET_USERS_BY_ID_SUCCESS,
-         payload: response.data.name,
+         payload: userName,
        });
      } catch (error) {
-       // Dispatch GET_USERS_BY_ID_FAIL action with the error message as the payload
+    //    Dispatch GET_USERS_BY_ID_FAIL action with the error message as the payload
        store.dispatch({
          type: GET_USERS_BY_ID_FAIL,
          payload: error.message,
